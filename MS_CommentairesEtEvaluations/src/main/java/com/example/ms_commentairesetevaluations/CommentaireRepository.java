f465f0bcfd179373f7e4efeaad16c4372b8721eb
nesrine1703 <nesrine.werfelli@esprit.tn>
2024-04-11 17:57:12 +0100

    delete

--- MS_CommentairesEtEvaluations/src/main/java/com/example/ms_commentairesetevaluations/CommentaireRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.ms_commentairesetevaluations;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommentaireRepository extends JpaRepository<Commentaire, Integer> {
-}

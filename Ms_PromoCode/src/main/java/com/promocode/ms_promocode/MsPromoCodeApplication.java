bad8064dd65ae91f623bd808e90097ae8c9f3a2c
messid <manar.essid@sofrecom.com>
2024-04-12 03:10:52 +0100

    first commit

---
+++ Ms_PromoCode/src/main/java/com/promocode/ms_promocode/MsPromoCodeApplication.java
@@ -0,0 +1,12 @@
+package com.promocode.ms_promocode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class MsPromoCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsPromoCodeApplication.class, args);
+    }
+
+}
---
+++ Ms_PromoCode/src/test/java/com/promocode/ms_promocode/MsPromoCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promocode.ms_promocode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPromoCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
